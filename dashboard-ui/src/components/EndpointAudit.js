6dff8ead581edd644c59d02fd4efc983918042b5
simon jung <drinkwater0502@gmail.com>
2023-11-30 08:53:24 -0800

    more audit wtf man

--- dashboard-ui/src/components/EndpointAudit.js
+++ dashboard-ui/src/components/EndpointAudit.js
@@ -9,7 +9,7 @@ export default function EndpointAudit(props) {
     const [index, setIndex] = useState(null);
 
     const getAudit = () => {
-        fetch(`http://ec2-52-10-1-233.us-west-2.compute.amazonaws.com/audit/${props.endpoint}?index=${rand_val}`)
+        fetch(`http://ec2-52-10-1-233.us-west-2.compute.amazonaws.com/audit_log/${props.endpoint}?index=${rand_val}`)
             .then(res => res.json())
             .then((result)=>{
 				console.log("Received Audit Results for " + props.endpoint)
